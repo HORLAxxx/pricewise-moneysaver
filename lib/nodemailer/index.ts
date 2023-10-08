8fefa39956a7c1773ee813d9be6bb94af9971e8e
HORLAxxx <olaitansonaike39@gmail.com>
2023-10-08 15:42:52 +0100

    fix build errors

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,7 +1,7 @@
 
 "use server"
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-//process.env.NODE_DEBUG = 'ssl';
+//process.env.NODE_DEBUG = 'ssl'; aaddd
 
 import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
 import nodemailer from 'nodemailer';
